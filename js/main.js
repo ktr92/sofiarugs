11507b97406f231cbf5a129ac554b4bcd18babbf
ktr92 <kudashevtim@gmail.com>
2021-02-22 16:33:05 +0300

    start

--- js/main.js
+++ js/main.js
@@ -177,6 +177,28 @@ $(document).ready(function() {
 	});
 	
 	
+	$('form .rating').mousemove(function(e)
+	{
+		var stars = parseInt((e.pageX-$(this).offset().left)/19)*19+19;
+		$(this).children('span').css('width',stars);
+	});
+	$('form .rating').mouseout(function(e)
+	{
+		if ($(this).text()) {
+			$(this).children('span').css('width',$(this).text()*19);
+		}
+		else {
+			$(this).children('span').css('width','96px');
+		}
+	});
+	$('form .rating').click(function(e)
+	{
+		var stars = parseInt((e.pageX-$(this).offset().left)/19)+1;
+		$('form input[name="rating"]').val(stars);
+		$('form .rating span').html(stars);
+	});
+	
+	
 	
 	
 	function incrementValue(e) {
@@ -209,5 +231,55 @@ $(document).ready(function() {
 	});
 	
 });		
+	
+
+
+
+$(document).ready( function() {
+	var fnames = "";
+	var fname = 0;
+	function removeFiles() {
+			let rem = $(this).parent().attr('id');
+			$('.' + rem).remove();
+			$(this).parent().remove();
+			fnames = $('.filenames').html();
+	}
+	function updateFiles() {
+		fname = fname + 1;
+		 $(this).parent().parent().parent().parent().parent().append('<div class="fileupload"><label><div class="fileupload__files"><div class="fileupload__file"><input class="initinputfile" type="file" name="file_upload[]" multiple=""><span class="filebutton">Добавить фото</span></div></div></label></div>');
+		/* $('.filename').empty();	 */
+		if (this.files[0]) {
+			for (var i = 0; i < this.files.length; ++i) {
+				var filename = this.files.item(i).name.replace(/.*\\/, ""); 
+			/* 	$(this).siblings('.filenames').find('.filenamedef').append('<span class="filename"> ' + filename + ' ' + ' </span>'); */
+				fnames += "<span id='fname'" + fname +"> " + "<a href='#'>" + filename + "</a>" + " (" +Math.round(this.files.item(i).size/1024) + " кб)" + "<button><img src='/images/remove2.png'></button></span>";
+				$('.filenames').html(fnames);
+			}
+			/* $(this).siblings('span').empty(); */
+			$('.file-error').html("");
+			$('.file-upload span').css('text-transform', 'none');
+			$('.changefile').css('display', 'block');
+		}
+		console.log(fnames);
+		var newinput = document.querySelectorAll('.fileupload input[type=file]');
+		for (var i = 0; i < newinput.length; i++) {
+			newinput[i].addEventListener('change', updateFiles);
+		}
+		var newflnms = document.querySelectorAll('.filenames span button');
+		for (var i = 0; i < newflnms.length; i++) {
+				newflnms[i].addEventListener('click', removeFiles);
+			}
+		 $(this).parent().parent().parent().parent().addClass("fname" + fname).hide();
+			}
+			var input = document.querySelectorAll('.fileupload input[type=file]');
+			for (var i = 0; i < input.length; i++) {
+					input[i].addEventListener('change', updateFiles);
+				}
+			var flnms = document.querySelectorAll('.filenames span button');
+			for (var i = 0; i < flnms.length; i++) {
+					flnms[i].addEventListener('click', removeFiles);
+				}
+		});
 		
+			
 		
